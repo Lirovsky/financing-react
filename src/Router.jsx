a875074a5b2a9d4fbec75195c7fd0ec7e381bcb3
Lirohin <pablolangerprofissional@gmail.com>
2024-07-19 20:15:11 -0300

    Att Route

--- src/Router.jsx
+++ src/Router.jsx
@@ -11,7 +11,7 @@ export function Router() {
     return (
         <Routes>
             <Route path='/financing-react' element={<DefaultLayout />}>
-                <Route path='/' element={<Home />} />
+                <Route path='/financing-react' element={<Home />} />
                 <Route path='/blog' element={<Blog />} />
                 <Route path='/financing-react/services' element={<Services />} />
                 <Route path='/financing-react/about' element={<About />} />
