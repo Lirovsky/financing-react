4f6db77373d816f6792daccd34bbe95cb905f643
Lirohin <pablolangerprofissional@gmail.com>
2024-07-22 09:15:40 -0300

    subindo projeto

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "financing-react",
+  base: "/financing-react/",
 })
