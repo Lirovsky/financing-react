4b94e00681af36010b465d45290a17fb6a19c408
Lirovsky <pablolangerprofissional@gmail.com>
2025-02-13 09:17:11 -0300

    Atualização fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'filter': "linear-gradient(rgba(48, 110, 232, .8), rgba(48, 110, 232, .9)), url('src/img/header2.jpg')",
-     },
+        filter:
+          "linear-gradient(rgba(48, 110, 232, .8), rgba(48, 110, 232, .9)), url('src/img/header2.jpg')",
+      },
+      fontFamily: {
+        worksans: ['"Work Sans"', "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
